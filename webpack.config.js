aec66893f5e1450bfb14a3afef4543ba4fc1a0b0
zuffidries <zuffidries@gmail.com>
2018-12-12 21:59:18 -0500

    CONFIG FILES

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const env = process.env.NODE_ENV || 'development';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// set to 'production' or 'development' in your env
+
+module.exports = {
+  mode: env,
+  entry: ['babel-polyfill', './src'], // this is where our app lives
+  devtool: 'source-map', // this enables debugging with source in chrome devtools
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [ //here's the part you want
+          { loader: 'babel-loader'},
+          { loader: 'eslint-loader' }
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+    }),
+  ],
+};
