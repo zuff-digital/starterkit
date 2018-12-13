889523d07d4b57c0f9f3f3eaa86bf93f70ff02a2
zuffidries <zuffidries@gmail.com>
2018-12-12 22:23:37 -0500

    SASS + IMAGE-LOADING

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import $ from 'jquery';
+import './style.scss';
 
 $('#main').html('SKEDDIAMO!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const env = process.env.NODE_ENV || 'development';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // set to 'production' or 'development' in your env
+
+const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { loader: 'style-loader' };
+
 
 module.exports = {
   mode: env,
@@ -16,9 +20,40 @@ module.exports = {
           { loader: 'eslint-loader' }
         ]
       },
+      {
+        test: /\.s?css/,
+        use: [
+          finalCSSLoader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
     template: './src/index.html',
     filename: './index.html',
